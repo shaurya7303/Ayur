a3d0ad2ec0aae44d2e8e5eefbc3b714cb1f9d0d2
shaurya7303 <shauryassinghh@gmail.com>
2025-11-17 15:42:43 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Ayur",
+    version="0.1.0",
+    author="Shaurya Singh",
+    author_email="shauryassinghh@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+    )
